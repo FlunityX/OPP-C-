6297eda68cc6f545fb23c0011b4f09be0e5b909e
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-12-04 17:59:01 +0700

    căn bản về lớp và contructor

---
+++ so_phuc/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp17;
+
+namespace so_phuc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sophuc sp1 = new sophuc(3,4);
+            sp1.xuat();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ so_phuc/sophuc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class sophuc
+    {
+        public double phanthuc;
+        public double phanao;
+        public sophuc(double phanthuc,double phanao) 
+        {
+            this.phanthuc = phanthuc;
+            this.phanao = phanao;
+        }
+        public void xuat()
+        {
+            Console.WriteLine(" so phuc cua ban la : {0} + {1}i",phanthuc,phanao);
+        } 
+    }
+}
