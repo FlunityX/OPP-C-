c0967a47ccbd19d9844a6680646e0c0c77451686
FlunityX <149858650+FlunityX@users.noreply.github.com>
2024-01-03 23:02:22 +0700

    biến tĩnh và phương thức tĩnh

---
+++ biến_tĩnh_phương_thức_tĩnh/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace biến_tĩnh_phương_thức_tĩnh
+{
+    public class baitap_static
+    {
+        public static int count = 0;
+        public static void Count()
+        {
+            count++;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        baitap_static dtg1 = new baitap_static();
+        baitap_static.Count();
+        baitap_static dtg2 =new baitap_static();
+        baitap_static.Count();
+        Console.WriteLine("co tat ca :"  + baitap_static.count+ " doi tuong dc khoi tao");
+        Console.ReadLine();
+
+        }
+    }
+}
