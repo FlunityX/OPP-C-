2508f3648f358564b8a6b7102a142aa315e17c11
FlunityX <149858650+FlunityX@users.noreply.github.com>
2024-01-21 01:13:19 +0700

    ôn lại DI

---
+++ Dependency injection (DI)/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_injection__DI_
+{
+    public interface IEngine
+    {
+        void Start();
+    }
+    public class GasolineEngine : IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Gasoline engine started");
+        }
+    }
+    public class Car
+    {
+        // _engine là 1 đối tg của interface
+        private readonly IEngine _engine;
+
+        // Constructor Injection
+        public Car(IEngine engine)
+        {
+            _engine = engine;
+        }
+
+        public void Start()
+        {
+            _engine.Start();
+            Console.WriteLine("Car started");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // tạo 1 đối tg interface engine 
+            IEngine engine = new GasolineEngine();
+            Car myCar = new Car(engine);
+
+            // Start the car
+            myCar.Start();
+            Console.ReadLine();
+        }
+    }
+}
