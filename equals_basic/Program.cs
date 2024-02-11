be846dbe904485b490948037db3d8fc86c396c77
nguyễn mạnh <manharia123@gmail.com>
2024-02-11 12:32:58 +0700

    phương thức có sẵn equals

---
+++ equals_basic/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace equals_basic
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override bool Equals(object? obj)
+        {
+            // nếu obj = null và obj khác kiểu dữ liệu của dối tg 
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            // ép kiểu object về class 
+            Person otherPerson = (Person)obj;
+            return Name == otherPerson.Name && Id == otherPerson.Id;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person { Name = "John", Id = 30 };
+            Person person2 = new Person { Name = "John", Id = 30 };
+            bool areEqual = person1.Equals(person2);
+            Console.WriteLine("Two persons are equal: " + areEqual);
+        }
+    }
+}
