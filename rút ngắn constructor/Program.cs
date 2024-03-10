1baa17a452e1be56de8b7d4efb4d85af28603fa3
nguyễn mạnh <manharia123@gmail.com>
2024-03-10 10:18:25 +0700

    rút ngắn lệnh contructor

---
+++ rút ngắn constructor/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace rút_ngắn_constructor
+{
+    /* public class User
+     {
+         /* cách 1 :
+         public int age { get; set;}
+         public string name { get; set;}
+         public User(string  name, int age)
+         {
+             this.age= age;
+             this.name = name;
+         }
+        }
+     cách 2:*/
+    public class User(string name , string lastName)
+    {
+        public string FirstName { get; } = name;
+        public string LastName { get; } = lastName;
+        public void print()
+        {
+            Console.WriteLine($"My Firts Name is {name} and Last name is  {LastName}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("nguyen","manh");
+            user.print();
+            Console.ReadLine();
+
+        }
+    }
+}
