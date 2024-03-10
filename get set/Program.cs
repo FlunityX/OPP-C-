b7614c65b8ed46f06b135663a0ff23f139b884c6
nguyễn mạnh <manharia123@gmail.com>
2024-03-10 10:20:48 +0700

    Get Set trường dữ liệu

---
+++ get set/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace get_set
+{
+    public class test
+    {
+        // có 4 cách get set
+        // 1. ghi trực tiếp
+        public bool isset { get; set; }
+        public bool issethide { get; private set; }
+        //2. trả về giá trị của biến private 
+        private bool _is;
+        public bool isSet
+        {
+            get
+            {
+                return _is;
+            }
+            set
+            {
+                isset = value;
+            }
+        }
+        // 3.dùng lambda để get set 
+        private bool s;
+        public bool S { get => s; set => s = value; }
+        // 4. nếu chỉ có get
+        private bool e;
+        public bool V => e;
+
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
