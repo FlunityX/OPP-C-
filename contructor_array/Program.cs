7b22b131b1cf116cd2c2191b22d22c770b5cffc9
nguyễn mạnh <manharia123@gmail.com>
2024-03-17 15:02:07 +0700

    Array Contructor

---
+++ contructor_array/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace contructor_array
+{
+    class contructor_array
+    {
+        private int[] array;
+        public int[] Array { get { return array; } set { array = value; } }
+        public contructor_array(int[] array)
+        {
+            this.Array = array;
+        }
+        public void Print(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($" phan tu thu {i+1} la : {array[i]}");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = new int[] { 1, 2, 3 };
+            contructor_array _Array = new contructor_array(values);
+            _Array.Print(values);
+            Console.ReadLine();
+        }
+    }
+}
